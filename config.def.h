a68ebaddce6f5b1001350fc2442328503357ca68
jkoz <phuoctaitp@gmail.com>
2014-11-30 14:59:47 -0700

    add xft patch

--- config.def.h
+++ config.def.h
@@ -4,7 +4,7 @@
 /* Default settings; can be overrided by command line. */
 
 static Bool topbar = True;                  /* -b  option; if False, dmenu appears at bottom */
-static const char *font = NULL;             /* -fn option; default X11 font or font set      */
+static const char *font = "Source Code Pro:size=10:antialias=true"; /* -fn option; default X11 font or font set      */
 static const char *prompt = NULL;           /* -p  option; prompt to the elft of input field */
 static const char *normbgcolor = "#222222"; /* -nb option; normal background                 */
 static const char *normfgcolor = "#bbbbbb"; /* -nf option; normal foreground                 */
