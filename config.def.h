aae28cd2215f6a83072af1087e8acad34eaf0931
jkoz <phuoctaitp@gmail.com>
2014-11-30 11:16:14 -0700

    use Xresouce

--- config.def.h
+++ config.def.h
@@ -6,10 +6,10 @@
 static Bool topbar = True;                  /* -b  option; if False, dmenu appears at bottom */
 static const char *font = NULL;             /* -fn option; default X11 font or font set      */
 static const char *prompt = NULL;           /* -p  option; prompt to the elft of input field */
-static const char *normbgcolor = "#222222"; /* -nb option; normal background                 */
-static const char *normfgcolor = "#bbbbbb"; /* -nf option; normal foreground                 */
-static const char *selbgcolor  = "#005577"; /* -sb option; selected background               */
-static const char *selfgcolor  = "#eeeeee"; /* -sf option; selected foreground               */
+static const char *normbgcolor = NULL; /* -nb option; normal background                 */
+static const char *normfgcolor = NULL; /* -nf option; normal foreground                 */
+static const char *selbgcolor  = NULL; /* -sb option; selected background               */
+static const char *selfgcolor  = NULL; /* -sf option; selected foreground               */
 static const char *outbgcolor  = "#00ffff";
 static const char *outfgcolor  = "#000000";
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
